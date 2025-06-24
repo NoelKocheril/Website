f98e2978c5cb46c808a91a2091213b934e309f6b
Noel Kocheril <10156492+NoelKocheril@users.noreply.github.com>
2025-06-23 22:30:37 -0400

    Add Nuxt Eslint and Nuxt Content Modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxt/content', '@nuxt/eslint'],
+  eslint: {
+
+  },
 })
